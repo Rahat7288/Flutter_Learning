c2bf4bd5bf2d014d66fa51241ecccf97008c93c2
Rahat <rahatislamakash@gmail.com>
2022-02-22 00:34:20 +0600

    adding statefull weidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,114 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: Card(),
 ));
 
+class Card extends StatefulWidget {
 
+  @override
+  State<Card> createState() => _CardState();
+}
 
-class Home extends StatelessWidget {
-
+class _CardState extends State<Card> {
+  int id = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[200],
       appBar: AppBar(
-        title: const Text('Test 2 '),
+        title: Text('Id Card'),
         centerTitle: true,
-        backgroundColor: Colors.teal[200],
+        backgroundColor: Colors.purple,
+        elevation: 0.0,
       ),
-
-      //writing column practice for flutter.
-      body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-              child:Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.lightGreen,
-                child: Text('1'),
-              ),
-          ),
-
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.deepOrangeAccent,
-              child: Text('2'),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.cyan,
-                child: Text('3'),
-              ),
-          ),
-
-        ],
-      ), 
-          
-
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          
+          setState(() {
+            id += 1;
+          });
         },
-        child: const Text('click me'),
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/cv.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.white,
+            ),
+            Text(
+              'Name:',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Rahat Islam Akash',
+              style: TextStyle(
+                color: Colors.black87,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Id:',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              '$id',
+              style: TextStyle(
+                color: Colors.black87,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'rahatislamakash@gmail.com',
 
-      );
+                ),
+              ],
+            ),
 
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
