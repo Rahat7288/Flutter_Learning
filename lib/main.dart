35bcf70bb440e2382432c52d6ebe16af1f39f238
Rahat <rahatislamakash@gmail.com>
2022-02-23 22:54:16 +0600

    listView in class

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Card(),
+void main()=> runApp(MaterialApp(
+  home: QoutList(),
 ));
 
-class Card extends StatefulWidget {
+class QoutList extends StatefulWidget {
+
 
   @override
-  State<Card> createState() => _CardState();
+  _QoutListState createState() => _QoutListState();
 }
 
-class _CardState extends State<Card> {
-  int id = 0;
+class _QoutListState extends State<QoutList> {
+
+  List<String> qoutes = [
+    'Be Yourself','everyone else is already taken',
+    'i have nothing to declare except my genius',
+    'the truth is rarely pure and never simple'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[200],
+      backgroundColor: Colors.white24,
       appBar: AppBar(
-        title: Text('Id Card'),
+        title: Text("Awesome Qoutes"),
         centerTitle: true,
-        backgroundColor: Colors.purple,
-        elevation: 0.0,
+        backgroundColor: Colors.black26,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            id += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.purple,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/cv.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.white,
-            ),
-            Text(
-              'Name:',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0,),
-            Text(
-              'Rahat Islam Akash',
-              style: TextStyle(
-                color: Colors.black87,
-                letterSpacing: 2.0,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0,),
-            Text(
-              'Id:',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0,),
-            Text(
-              '$id',
-              style: TextStyle(
-                color: Colors.black87,
-                letterSpacing: 2.0,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0,),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  'rahatislamakash@gmail.com',
-
-                ),
-              ],
-            ),
-
-
-
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: qoutes.map((quote){
+          return Text(quote);
+        }).toList(),
       ),
     );
   }
 }
-
-
-
